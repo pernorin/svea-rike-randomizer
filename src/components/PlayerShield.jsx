e9923dd9d46c1bb954e3c69359554316afdf23ce
Per Norin <pelle.norin.79@gmail.com>
2025-09-11 18:37:44 +0200

    Moved images

--- src/components/PlayerShield.jsx
+++ src/components/PlayerShield.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 //import image from '../assets/Brahe.png'
 
-export default function PlayerShield({ player, hide }) {
+export default function PlayerShield({ playerName, playerStatus}) {
   // const imagePath = `../assets/${player}.png`;
+  // console.log('render shield');
+ 
   
   return (
-    <div className={hide ? 'hide' : undefined}>
+    // <div className={hide ? 'hide' : undefined}>
+    <div className={playerStatus}>
       {/* <img src={require(`../assets/${player}.png`)} alt='' height='100' /> */}
-      <img src={`${player}.png`} alt='' height='60' />      
-      {player}
+      <img src={`../src/assets/${playerName}.png`} alt='' height='60' />      
+      {playerName} {playerStatus}      
     </div>
   );
 }
+// export default function PlayerShield({ player, hide, index }) {
+//   // const imagePath = `../assets/${player}.png`;
+//   // console.log('render shield');
+  
+//   let className = player.status
+  
+//   return (
+//     // <div className={hide ? 'hide' : undefined}>
+//     <div className={cl}>
+//       {/* <img src={require(`../assets/${player}.png`)} alt='' height='100' /> */}
+//       <img src={`${player.name}.png`} alt='' height='60' />      
+//       {player.name} {player.status}
+//     </div>
+//   );
+// }
